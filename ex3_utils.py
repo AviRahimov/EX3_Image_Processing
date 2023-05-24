66b16ef424fa3db49c8a06a9fa9434f8c5472c20
Avi Rahimov <rahimovavi3@gmail.com>
2023-05-25 01:46:02 +0300

    finish the first function

--- ex3_utils.py
+++ ex3_utils.py
@@ -26,18 +26,6 @@ def myID() -> np.int:
 # ------------------------ Lucas Kanade optical flow ------------------------
 # ---------------------------------------------------------------------------
 
-def myOpticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
-                  win_size=5) -> (np.ndarray, np.ndarray):
-    """
-    Given two images, returns the Translation from im1 to im2
-    :param im1: Image 1
-    :param im2: Image 2
-    :param step_size: The image sample size
-    :param win_size: The optical flow window size (odd number)
-    :return: Original points [[x,y]...], [[dU,dV]...] for each points
-    """
-
-
 def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
                 win_size=5) -> (np.ndarray, np.ndarray):
     """
@@ -48,7 +36,48 @@ def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
     :param win_size: The optical flow window size (odd number)
     :return: Original points [[x,y]...], [[dU,dV]...] for each points
     """
+    # X and Y kernel gradients to convolve with im2 image
+    x_deriv_kernel = np.array([1, 0, -1])
+    y_deriv_kernel = x_deriv_kernel.T
+    # Calculating the gradients for x and y and the difference between the times of im2 and im1
+    Ix = cv2.filter2D(im2, -1, x_deriv_kernel, borderType=cv2.BORDER_REPLICATE)
+    Iy = cv2.filter2D(im2, -1, y_deriv_kernel, borderType=cv2.BORDER_REPLICATE)
+    It = im2 - im1
 
+    # calculating the half size of the window such that we can run over all the window in the for loop
+    half_window_size = win_size//2
+    # Initializing the lists for the good original point and the vector for each point, by saying good point I meant
+    # to the points that are following after 2 statements of the eigen-values represented in the PDF.
+    original_points = []
+    vector_for_points = []
+
+    # Traverse on all im1 with windowed size and calculating A(transpose)A = A(transpose)b
+    for i in range(half_window_size, im1.shape[0] - half_window_size + 1, step_size):
+        for j in range(half_window_size, im1.shape[1] - half_window_size + 1, step_size):
+            start_rows_window = i - half_window_size
+            end_rows_window = i + half_window_size + 1
+            start_cols_window = j - half_window_size
+            end_cols_window = j + half_window_size + 1
+            Ix_window = Ix[start_cols_window : end_cols_window, start_cols_window : end_cols_window].flatten()
+            Iy_window = Iy[start_cols_window : end_cols_window, start_cols_window : end_cols_window].flatten()
+            It_window = It[start_cols_window : end_cols_window, start_cols_window : end_cols_window].flatten()
+            AT_A = [[(Ix * Ix).sum(), (Ix * Iy).sum()],
+                    [(Ix * Iy).sum(), (Iy * Iy).sum()]]
+            eigen_values = np.linalg.eigvals(AT_A)
+            lamda1 = eigen_values.min()
+            lamda2 = eigen_values.max()
+            # Checking if the original point is good or not by 2 statements of the eigen-values
+            if lamda2 <= 1 or lamda1 / lamda2 >= 100:
+                continue
+            # If the eigen-values are good so, we can continue to calculate A(transpose) * b
+            AT_b = [[(Ix_window * It_window).sum()],
+                    [(Iy_window * It_window).sum()]]
+            # As A(transpose) * A is a squared matrix, we can calculate the inverse of it and multiply it from the left
+            # ,and then we get u and v i.e. the vectors for the good original points i and j.
+            u_v = np.linalg.inv(AT_A) @ AT_b
+            vector_for_points.append(u_v)
+            original_points.append([i, j])
+        return original_points, vector_for_points
 
 def opticalFlowPyrLK(img1: np.ndarray, img2: np.ndarray, k: int,
                      stepSize: int, winSize: int) -> np.ndarray:
@@ -68,22 +97,22 @@ def opticalFlowPyrLK(img1: np.ndarray, img2: np.ndarray, k: int,
 # ---------------------------------------------------------------------------
 
 
-def find_vec_of_transformMed(mat: np.ndarray):
-    """
-    This method calculates the median of the (u,v) vector from @mat.
-    :param mat: array of the vectors of the OP
-    :return: the median vector - (u,v)
-    """
-
-
-def find_vec_of_transform(img1: np.ndarray, img2: np.ndarray, mat: np.ndarray):
-    """
-    This function returns best vector that suits the translation between @img1 to @img2.
-    :param img1: first image
-    :param img2: second image
-    :param mat: array of the vectors of the OP
-    :return: vector - [u,v]
-    """
+# def find_vec_of_transformMed(mat: np.ndarray):
+#     """
+#     This method calculates the median of the (u,v) vector from @mat.
+#     :param mat: array of the vectors of the OP
+#     :return: the median vector - (u,v)
+#     """
+#
+#
+# def find_vec_of_transform(img1: np.ndarray, img2: np.ndarray, mat: np.ndarray):
+#     """
+#     This function returns best vector that suits the translation between @img1 to @img2.
+#     :param img1: first image
+#     :param img2: second image
+#     :param mat: array of the vectors of the OP
+#     :return: vector - [u,v]
+#     """
 
 
 def findTranslationLK(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
@@ -94,13 +123,13 @@ def findTranslationLK(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
     """
 
 
-def bestAngle(img1: np.ndarray, img2: np.ndarray) -> float:
-    """
-    This function go over all the possibilities for an angle between two images (0-359).
-    :param img1: first image
-    :param img2: second image
-    :return: the best angle
-    """
+# def bestAngle(img1: np.ndarray, img2: np.ndarray) -> float:
+#     """
+#     This function go over all the possibilities for an angle between two images (0-359).
+#     :param img1: first image
+#     :param img2: second image
+#     :return: the best angle
+#     """
 
 
 def findRigidLK(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
@@ -111,14 +140,14 @@ def findRigidLK(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
     """
 
 
-def findCorrelation(img1: np.ndarray, img2: np.ndarray):
-    """
-    This function looks for two points, one from @img1 and second from @img2.
-    The two points are the ones with the highest correlation.
-    :param img1: first image
-    :param img2: second image
-    :return: 2 points - x1, y1, x2, y2
-    """
+# def findCorrelation(img1: np.ndarray, img2: np.ndarray):
+#     """
+#     This function looks for two points, one from @img1 and second from @img2.
+#     The two points are the ones with the highest correlation.
+#     :param img1: first image
+#     :param img2: second image
+#     :return: 2 points - x1, y1, x2, y2
+#     """
 
 
 def findTranslationCorr(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
@@ -129,14 +158,14 @@ def findTranslationCorr(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
     """
 
 
-def getAngle(point1, point2):
-    """
-    This function calculate the angle between @point1 to @point2 by checking the intersection
-    point of these points by creating two lines to the mass of center (0,0).
-    :param point1: [x,y]
-    :param point2: [x,y]
-    :return: float - angle
-    """
+# def getAngle(point1, point2):
+#     """
+#     This function calculate the angle between @point1 to @point2 by checking the intersection
+#     point of these points by creating two lines to the mass of center (0,0).
+#     :param point1: [x,y]
+#     :param point2: [x,y]
+#     :return: float - angle
+#     """
 
 
 def findRigidCorr(im1: np.ndarray, im2: np.ndarray) -> np.ndarray:
@@ -156,7 +185,19 @@ def warpImages(im1: np.ndarray, im2: np.ndarray, T: np.ndarray) -> np.ndarray:
     :return: warp image 2 according to T and display both image1
     and the wrapped version of the image2 in the same figure.
     """
+    height, weight = im2.shape
 
+    X, Y = np.meshgrid(range(weight), range(height))
+    new_img = np.array([X.flatten(), Y.flatten(), np.ones_like(X.flatten())])
+
+    old_img = (np.linalg.inv(T)) @ new_img
+    first_mask = (old_img[0, :] > weight) | (old_img[0, :] < 0)
+    second_mask = (old_img[1, :] > height) | (old_img[1, :] < 0)
+    old_img[0, :][first_mask] = 0
+    old_img[1, :][second_mask] = 0
+    transformed_img = im2[old_img[1, :].astype(int), old_img[0, :].astype(int)]
+
+    return transformed_img
 
 # ---------------------------------------------------------------------------
 # --------------------- Gaussian and Laplacian Pyramids ---------------------
@@ -189,13 +230,13 @@ def gaussianPyr(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
     """
 
 
-def gaussExpand(img: np.ndarray, gs_k: np.ndarray) -> np.ndarray:
-    """
-    Expands a Gaussian pyramid level one step up
-    :param img: Pyramid image at a certain level
-    :param gs_k: The kernel to use in expanding
-    :return: The expanded level
-    """
+# def gaussExpand(img: np.ndarray, gs_k: np.ndarray) -> np.ndarray:
+#     """
+#     Expands a Gaussian pyramid level one step up
+#     :param img: Pyramid image at a certain level
+#     :param gs_k: The kernel to use in expanding
+#     :return: The expanded level
+#     """
 
 
 def pyrBlend(img_1: np.ndarray, img_2: np.ndarray, mask: np.ndarray, levels: int) -> (np.ndarray, np.ndarray):
